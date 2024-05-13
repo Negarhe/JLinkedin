20f6eb44e504ee039dc7b983d07fbfa4cc6bea9b
Negar Hedayati <negarhy88@gmail.com>
2024-05-13 10:05:14 +0330

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("1. Sign up");
+        System.out.println("2. Login");
+        System.out.println("3. exit");
+
+        int choice = in.nextInt();
+        in.nextLine();
+        String command = in.nextLine();
+
+        if (choice == 1) {
+            System.out.println("Enter your email: ");
+            String email = in.nextLine();
+            if(email.contains("@") && email.contains(".")){
+                System.out.println("Enter your name: ");
+                String name = in.nextLine();
+                System.out.println("Enter your last name: ");
+                String lastName = in.nextLine();
+                System.out.println("Enter your password: ");
+                String password = in.nextLine();
+            }
+            else{
+                System.out.println("Invalid email");
+            }
+        }
+
+        else if(choice == 2){
+
+        }
+
+        else if(choice == 3){
+            System.exit(0);
+        }
+
+    }
+}
