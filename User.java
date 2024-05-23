c3db1b66e9a2efbdcdd840a590af0e20bd53bb73
Negar Hedayati <negarhy88@gmail.com>
2024-05-23 18:37:19 +0330

    Update User.java

--- User.java
+++ User.java
@@ -1,3 +1,4 @@
+import javax.xml.crypto.Data;
 import java.util.*;
 
 public class User {
@@ -12,6 +13,8 @@ public class User {
     private String country ; //60 character
     private ContactInformation contactInfo ; //phone number, address, birthday, relationship status
     //two fields for profile photo and background photo
+    private String profilePhoto ; //link to the photo
+    private String backgroundPhoto ; //link to the photo
 
     private String profession ; //60 character, for example "software engineering"
     private Status status ; //seeking job? hiring? providing services?
@@ -33,6 +36,17 @@ public class User {
             this.name = name;
             this.lastName = lastName;
             this.password = password;
+            this.title = "";
+            this.additionalName = "";
+            this.city = "";
+            this.country = "";
+            this.contactInfo = new ContactInformation("", "", ContactInformation.Kind.MOBILE, "", "", "");
+            this.profilePhoto = "";
+            this.backgroundPhoto = "";
+            this.profession = "";
+            this.status = Status.HIRING;
+            this.experiences = new ArrayList<>();
+            this.educations = new ArrayList<>();
         }
         else
             System.out.println("Invalid email");
@@ -43,10 +57,129 @@ public class User {
     public boolean emailValidityCheck(String email) {
         return email.contains("@") && email.contains(".");
     }
-    public void displayProfile() {
+    public void displayProfile(String email) {
+        DataBase dataBase = new DataBase();
+        User user = dataBase.getUser(email);
+        System.out.println("Name: " + user.getName());
+        System.out.println("Last name: " + user.getLastName());
+        System.out.println("Email: " + user.getEmail());
+        System.out.println("Title: " + user.getTitle());
+        System.out.println("Additional name: " + user.getAdditionalName());
+        System.out.println("City: " + user.getCity());
+        System.out.println("Country: " + user.getCountry());
+        System.out.println("Contact information: " + user.getContactInfo());
+        System.out.println("Profession: " + user.getProfession());
+        System.out.println("Status: " + user.getStatus());
+        System.out.print("Experiences: ");
+        for (Job job : user.getExperiences()) {
+            System.out.print(job + ", ");
+        }
+
+        System.out.print("Educations: ");
+        for (Education education : user.getEducations()) {
+            System.out.print(education + ", ");
+        }
+
+        System.out.println("Profile photo: " + user.getProfilePhoto());
 
     }
-    public void editProfile() {}
+    public void editProfile() {
+        DataBase dataBase = new DataBase();
+        System.out.println("What do you want to edit?");
+        System.out.println("1. Title");
+        System.out.println("2. Additional name");
+        System.out.println("3. City");
+        System.out.println("4. Country");
+        System.out.println("5. Contact information");
+        System.out.println("6. Profession");
+        System.out.println("7. Status");
+        System.out.println("8. Experiences");
+        System.out.println("9. Educations");
+        System.out.println("10. Profile photo");
+        System.out.println("11. Background photo");
+
+        int choice = Main.in.nextInt();
+        Main.in.nextLine();
+
+        switch (choice) {
+            case 1:
+                System.out.println("Enter the new title: ");
+                this.setTitle(Main.in.nextLine());
+                //set title in dataBase
+                dataBase.updateUserTitle(this.getEmail(), this.getTitle());
+                break;
+            case 2:
+                System.out.println("Enter the new additional name: ");
+                this.setAdditionalName(Main.in.nextLine());
+                //set additionalName in dataBase
+                dataBase.updateUserAdditionalName(this.getEmail(), this.getAdditionalName());
+                break;
+            case 3:
+                System.out.println("Enter the new city: ");
+                this.setCity(Main.in.nextLine());
+                //set city in dataBase
+                dataBase.updateUserCity(this.getEmail(), this.getCity());
+                break;
+            case 4:
+                System.out.println("Enter the new country: ");
+                this.setCountry(Main.in.nextLine());
+                //set country in dataBase
+                dataBase.updateUserCountry(this.getEmail(), this.getCountry());
+                break;
+            case 5:
+                System.out.println("Enter the new contact information: ");
+                System.out.println("Phone number: ");
+                String phoneNumber = Main.in.nextLine();
+                System.out.println("Kind: ");
+                System.out.println("1. Mobile");
+                System.out.println("2. Home");
+                System.out.println("3. Work");
+                int kindChoice = Main.in.nextInt();
+                Main.in.nextLine();
+                ContactInformation.Kind kind = ContactInformation.Kind.MOBILE;
+                switch (kindChoice) {
+                    case 1:
+                        kind = ContactInformation.Kind.MOBILE;
+                        break;
+                    case 2:
+                        kind = ContactInformation.Kind.HOME;
+                        break;
+                    case 3:
+                        kind = ContactInformation.Kind.JOB;
+                        break;
+                }
+                System.out.println("Address: ");
+                String address = Main.in.nextLine();
+                System.out.println("Birthday: ");
+                String birthday = Main.in.nextLine();
+                System.out.println("Relationship status: ");
+                String relationshipStatus = Main.in.nextLine();
+                this.setContactInfo(new ContactInformation(this.email, phoneNumber, kind, address, birthday, relationshipStatus));
+                break;
+            case 6:
+                System.out.println("Enter the new profession: ");
+                this.setProfession(Main.in.nextLine());
+                break;
+            case 7:
+                System.out.println("Enter the new status: ");
+                System.out.println("1. Seeking job");
+                System.out.println("2. Hiring");
+                System.out.println("3. Providing services");
+                int statusChoice = Main.in.nextInt();
+                Main.in.nextLine();
+                switch (statusChoice) {
+                    case 1:
+                        this.setStatus(Status.SEEKING_JOB);
+                        break;
+                    case 2:
+                        this.setStatus(Status.HIRING);
+                        break;
+                    case 3:
+                        this.setStatus(Status.PROVIDING_SERVICES);
+                        break;
+                }
+        }
+    }
 
 
 
@@ -89,6 +222,22 @@ public class User {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getProfilePhoto() {
+        return profilePhoto;
+    }
+
+    public void setProfilePhoto(String profilePhoto) {
+        this.profilePhoto = profilePhoto;
+    }
+
+    public String getBackgroundPhoto() {
+        return backgroundPhoto;
+    }
+
+    public void setBackgroundPhoto(String backgroundPhoto) {
+        this.backgroundPhoto = backgroundPhoto;
     }
 
     public String getAdditionalName() {
